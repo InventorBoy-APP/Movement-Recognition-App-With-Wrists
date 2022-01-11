5343704e3df6643f6fe1a146b96e3702d2100fa2
InventorBoy-APP <78768396+InventorBoy-APP@users.noreply.github.com>
2022-01-10 19:56:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-songOne = " ";
-songTwo = " ";
+songOne = "";
+songTwo = "";
 
+
+leftWristX = 0;
+leftWristY = 0;
+
+
+rightWristX = 0;
+rightWristY = 0;
+
+
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 
 function setup(){
      canvas = createCanvas(600,500);
      canvas.center();
 
+
      video = createCapture(VIDEO);
      video.hide();
+
+
+     poseNet = ml5.poseNet(video, modelLoaded); 
+     poseNet.on('pose', gotPoses);
 }
 
 
@@ -24,8 +40,32 @@ function preload(){
 
 
 function play(){
-    songTwo.play();
+    songOne.play();
 }
+
+
+function modelLoaded(){
+    console.log( " POSENET HAS BEEN INITIALIZED " );
+}
+
+
+function gotPoses(results){
+    if( results.length > 0){
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log(" Left wrist x = " + leftWristX + " Left wrist y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log( " Right wrist X = " + rightWristX + " Right wrist y = " + rightWristY);
+    }
+}
+
+ 
+
 
 
 
